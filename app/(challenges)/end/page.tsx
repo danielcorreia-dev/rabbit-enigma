4d609be050f30fc96a92ce6589eb4c1896801980
Daniel Correia <danielgalego.dev@gmail.com>
2024-01-05 21:15:14 -0300

    Refactor code to improve performance and readability

---
+++ app/(challenges)/end/page.tsx
@@ -0,0 +1,14 @@
+import EnigmaLayout from '@/app/components/EnigmaLayout';
+import React from 'react';
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return (
+    <EnigmaLayout backgroundColor="bg-white">
+      <div>you finally figured it out</div>
+    </EnigmaLayout>
+  );
+};
+
+export default Page;
