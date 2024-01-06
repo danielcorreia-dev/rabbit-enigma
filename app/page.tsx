efb449a28576d3a52f1807ffeb1ebf8c735b7cd2
Daniel Correia <danielgalego.dev@gmail.com>
2024-01-05 22:32:30 -0300

    fix: changing redirect to perm red

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
-import { redirect } from 'next/navigation';
+import { permanentRedirect } from 'next/navigation';
 import { title } from 'process';
 
 export default function Home() {
-  redirect('/journey');
+  permanentRedirect('/journey');
 }
